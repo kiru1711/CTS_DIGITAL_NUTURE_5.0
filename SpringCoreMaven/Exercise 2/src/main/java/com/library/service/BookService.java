81086838572153d3b4f094eb9972737912292d13
Kiruthika R <kiruthika.1171@gmail.com>
2026-06-30 11:18:05 +0530

    Added SpringCoreMaven Exercise 2

--- SpringCoreMaven/Exercise 2/src/main/java/com/library/service/BookService.java
+++ SpringCoreMaven/Exercise 2/src/main/java/com/library/service/BookService.java
@@ -6,12 +6,13 @@ public class BookService {
 
     private BookRepository bookRepository;
 
+    // Setter Injection
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    public void showService() {
-        System.out.println("Book Service called");
-        bookRepository.displayBook();
+    public void issueBook() {
+        System.out.println("Book Service started");
+        bookRepository.getBook();
     }
 }
