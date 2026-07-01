e99803f83d787af068f3ac77457a3aa4dfab64ce
Kiruthika R <kiruthika.1171@gmail.com>
2026-07-01 18:20:31 +0530

    Task 2 has been updated

--- SpringRest/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ SpringRest/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,22 +1,27 @@
 package com.cognizant.spring_learn;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringLearnApplication {
-
-	private static final Logger LOGGER =
-			LoggerFactory.getLogger(SpringLearnApplication.class);
-
-	public static void main(String[] args) {
-
-		LOGGER.info("SpringLearn Application Started");
-
+	public static void main(String[] args) throws Exception {
+		displayDate();
 		SpringApplication.run(SpringLearnApplication.class, args);
 
-		LOGGER.info("SpringLearn Application Running");
+
+	}
+	public static void displayDate() throws Exception {
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format =
+				context.getBean("dateFormat", SimpleDateFormat.class);
+		Date date = format.parse("31/12/2018");
+		System.out.println("Parsed Date: " + date);
 	}
 }
