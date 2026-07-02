be6279d702f40746430cfb6b5d6fd7a40a10e9aa
Kiruthika R <kiruthika.1171@gmail.com>
2026-07-02 12:10:45 +0530

    Country code implemented

--- SpringRest/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ SpringRest/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -1,22 +1,18 @@
 package com.cognizant.spring_learn.controller;
 
 import com.cognizant.spring_learn.Country;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cognizant.spring_learn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CountryController {
 
-    @RequestMapping("/country")
-    public Country getCountryIndia() {
+    @Autowired
+    CountryService countryService;
 
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
-
-        Country country = context.getBean("country", Country.class);
-
-        return country;
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
     }
 }
